b8fd6489727207d195eec725c8901c68624f97d1
MariaP-Ramalho <eduardapramalho@gmail.com>
2024-08-23 16:13:58 -0300

    fix: inputHandler

--- utils/InputHandler.java
+++ utils/InputHandler.java
@@ -30,11 +30,11 @@ public class InputHandler {
                 System.out.print("Insira o valor do livro: ");
                 double price = sc.nextDouble();
                 if (price <= 0) {
-                    throw new IllegalArgumentException("O valor do livro não pode ser negativo.");
+                    throw new IllegalArgumentException("O valor do livro não pode ser negativo. \n");
                 }
                 return price;
             } catch (InputMismatchException e) {
-                System.out.print("Por favor, insira um número válido para o preço.");
+                System.out.print("Por favor, insira um número válido para o preço. \n");
                 sc.nextLine();
             } catch (IllegalArgumentException e) {
                 System.out.print(e.getMessage());
@@ -43,9 +43,9 @@ public class InputHandler {
     }
 
     public String getBookPublisher() {
+        sc.nextLine();
         while(true){
             System.out.print("Insira a editora do livro: ");
-            sc.nextLine();
             String publisher = sc.nextLine();
             if (!publisher.isEmpty()) {
                 return publisher;
@@ -60,22 +60,20 @@ public class InputHandler {
         String anotherAuthor;
 
         do {
-            System.out.print("Insira o autor do livro: ");
-            String author = sc.nextLine();
-            if (author.isEmpty()) {
-                System.out.print("O nome do autor não pode ser vazio.");
-            } else {
-                authors.add(author);
-            }
+            do{
+                System.out.print("Insira o autor do livro: ");
+                String author = sc.nextLine();
+                if (author.isEmpty()) {
+                    System.out.print("O nome do autor não pode ser vazio. \n");
+                } else {
+                    authors.add(author);
+                }
+            }while(authors.isEmpty());
 
             System.out.print("Deseja inserir mais um autor? (S/N) ");
             anotherAuthor = sc.nextLine();
 
         } while (anotherAuthor.equalsIgnoreCase("S"));
-
-        if (authors.isEmpty()) {
-            throw new IllegalArgumentException("Pelo menos um autor deve ser inserido.");
-        }
 
         return authors;
     }
@@ -87,7 +85,7 @@ public class InputHandler {
             if (type.equalsIgnoreCase("D") || type.equalsIgnoreCase("F")) {
                 return type;
             } else {
-                System.out.print("Por favor, insira 'D' para digital ou 'F' para físico.");
+                System.out.print("Por favor, insira 'D' para digital ou 'F' para físico. \n");
             }
         }
     }
@@ -98,11 +96,11 @@ public class InputHandler {
                 System.out.print("Insira o tamanho do livro: ");
                 int size = sc.nextInt();
                 if (size <= 0) {
-                    throw new IllegalArgumentException("O tamanho do livro deve ser maior que zero.");
+                    throw new IllegalArgumentException("O tamanho do livro deve ser maior que zero. \n");
                 }
                 return size;
             } catch (InputMismatchException e) {
-                System.out.print("Por favor, insira um número válido para o tamanho.");
+                System.out.print("Por favor, insira um número válido para o tamanho. \n");
                 sc.nextLine();
             } catch (IllegalArgumentException e) {
                 System.out.print(e.getMessage());
@@ -116,11 +114,11 @@ public class InputHandler {
                 System.out.print("Insira o valor do frete: ");
                 float freight = sc.nextFloat();
                 if (freight < 0) {
-                    throw new IllegalArgumentException("O valor do frete não pode ser negativo.");
+                    throw new IllegalArgumentException("O valor do frete não pode ser negativo.\n ");
                 }
                 return freight;
             } catch (InputMismatchException e) {
-                System.out.print("Por favor, insira um número válido para o frete.");
+                System.out.print("Por favor, insira um número válido para o frete.\n");
                 sc.nextLine();
             } catch (IllegalArgumentException e) {
                 System.out.print(e.getMessage());
@@ -134,11 +132,11 @@ public class InputHandler {
                 System.out.print("Insira o número do estoque: ");
                 int stock = sc.nextInt();
                 if (stock < 0) {
-                    throw new IllegalArgumentException("O número do estoque não pode ser negativo.");
+                    throw new IllegalArgumentException("O número do estoque não pode ser negativo. \n");
                 }
                 return stock;
             } catch (InputMismatchException e) {
-                System.out.print("Por favor, insira um número válido para o estoque.");
+                System.out.print("Por favor, insira um número válido para o estoque. \n");
                 sc.nextLine();
             } catch (IllegalArgumentException e) {
                 System.out.print(e.getMessage());
